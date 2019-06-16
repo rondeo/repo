bb815d6048d2ecf7d72cab629f40e957d536c149
hulk38 <havanaroblejo@gmail.com>
2019-06-16 01:57:56 -0400

    Importando_webSockets

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const path = require('path');
 
 const express = require('express');
 const socketio = require('socket.io');
@@ -8,11 +9,9 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio.listen(server);
 
-io.on('connection', socket =>{
-    console.log('New Usuario!');
-})
+require('./sockets')(io);
 
-app.use(express.static('src/public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 server.listen(3000, () => {
     console.log('Server on Port 3000');
